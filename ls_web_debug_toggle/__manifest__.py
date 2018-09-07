d184566ae7e13bcdf438a68ec487a3ad2a25b20e
Gusti Tammam <gustitammam@gmail.com>
2018-09-07 13:36:48 +0700

    [10.0][ADD] Add new module: ls_web_debug_toggle

---
+++ ls_web_debug_toggle/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Linksoft Default Module Template",
+    'summary': """Put short summary here.""",
+    'author': "Linksoft Mitra Informatika",
+    'website': "https://linksoft.id",
+    # Check here: https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
+    'category': 'Uncategorized',
+    'version': '1.0.0',
+    'depends': [
+        # odoo addons
+        'base',
+        # third party addons
+
+        # developed addons
+
+    ],
+    # always loaded
+    'data': [
+        # group
+
+        # data
+
+        # action
+
+        # view
+
+        # menu
+
+        # security
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
