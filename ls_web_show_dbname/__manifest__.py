9cbb014e0ddff38d81f41108c174683b259b0b7f
Gusti Tammam <gustitammam@gmail.com>
2018-09-07 11:02:31 +0700

    [10.0][ADD] Add new module: ls_web_show_dbname

---
+++ ls_web_show_dbname/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Show Database Name on Hover",
+    'summary': """Show Database Name on Hover in Debug Mode""",
+    'author': "Linksoft Mitra Informatika",
+    'website': "https://linksoft.id",
+    # Check here: https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
+    'category': 'Extra Tools',
+    'version': '1.0.0',
+    'depends': [
+        # odoo addons
+        'base',
+        # third party addons
+
+        # developed addons
+
+    ],
+    # always loaded
+    'data': [
+        # group
+
+        # data
+
+        # action
+
+        # view
+
+        # menu
+
+        # security
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
---
+++ ls_web_show_dbname/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+
+__all__ = ['models']
